da3cb96311ed26059bf6cea85cf89b9fc1b3c56f
VENKAT RAM REDDY KUNTA <venkat.csulb@gmail.com>
2015-02-04 22:53:34 -0800

    Dynamic Programming : 1. Edit Distance 2. Knapsack

---
+++ editdistance/EditDistance.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problemsolving.editdistance;
+
+/**
+ *
+ * @author venkatramreddykunta
+ */
+public class EditDistance {
+
+    public EditDistance() {
+    }
+    
+    public static void main(String[] args) {
+        String u1="fast";
+        String v1="cats";
+        char[] result=new char[5];
+        
+        char[] u=u1.toCharArray();
+        char[] v=v1.toCharArray();
+        
+        int min=Integer.MAX_VALUE;
+        int[][] dp=new int[5][5];
+        
+        for (int i = 0; i < 5; i++) {
+            dp[i][0]=i;
+        }
+        for (int i = 0; i < 5; i++) {
+            dp[0][i]=i;
+        }
+//        for (int i = 0; i < 5; i++) {
+//            for (int j = 0; j < 5; j++) {
+//                System.out.print(" "+dp[i][j]);
+//            }
+//            System.out.println(" ");
+//        }
+        
+        for (int i = 1; i < 5; i++) {
+            for (int j = 1; j < 5; j++) {
+               min=Math.min(Math.min(dp[i-1][j-1], dp[i][j-1])  ,dp[i-1][j]);
+               if(u[i-1]==v[j-1]){
+                dp[i][j]=min;   
+               }else{
+                dp[i][j]=min+1;   
+               }
+            }
+        }
+        System.out.println(" DP ");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(" "+dp[i][j]);
+            }
+            System.out.println(" ");
+        }
+        
+        
+        System.out.println("Back tracking");
+        int i=u.length;
+        for(int j=v.length;j>0;){
+            if(u[i-1]==v[j-1]){
+                
+                if(dp[i][j]==dp[i-1][j-1]){
+                
+                    
+                    System.out.println("D "+u[i-1]+" - > "+v[j-1]);
+                    i--;j--;
+                }
+                else if(dp[i][j]==dp[i][j-1]){
+                
+                    System.out.println("L "+u[i-1]+" - > "+v[j-1]);
+                    j--;
+                }
+                else if(dp[i][j]==dp[i-1][j]){
+                    
+                    System.out.println("U "+u[i-1]+" - > "+v[j-1]);
+                i--;
+                }
+                
+            }
+            else{
+                
+                if(dp[i][j]==dp[i-1][j-1]+1){
+                    
+                    System.out.println("D "+u[i-1]+" - > "+v[j-1]);
+                    i--;j--;
+                }
+                else if(dp[i][j]==dp[i][j-1]){
+                    
+                    System.out.println("L "+u[i-1]+" - > "+v[j-1]);
+                j--;
+                }
+                else if(dp[i][j]==dp[i-1][j]){
+                
+                    System.out.println("U "+u[i-1]+" - > "+v[j-1]);
+                    i--;
+                }
+            }
+        }
+        
+        
+    }
+}
---
+++ knapsack/Knapsack.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problemsolving.knapsack;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author venkatramreddykunta
+ */
+public class Knapsack {
+
+    public Knapsack() {
+    
+    }
+    
+    public static void main(String[] args) {
+    int[][] knapsack=new int[7][9];
+    int weight[]={1,2,3,4,2,1};
+    int cost[]={2,4,5,5,2,3};
+    ArrayList<Integer> finalList=new ArrayList<>();
+    
+        for (int i = 0; i < 9; i++) {
+            knapsack[0][i]=0;
+        }
+        for (int i = 0; i < 7; i++) {
+            knapsack[i][0]=0;
+        }
+        System.out.println("Init ok ");
+        for (int i = 1; i < 7; i++) {
+//            System.out.println("i "+i);
+            for (int j = 1; j < 9; j++) {
+             //   System.out.println("i: "+i+", j: "+j+"weight[i]:"+weight[i-1]);
+                if(j-weight[i-1]>0)
+                    knapsack[i][j]=Math.max(knapsack[i-1][j], (knapsack[i-1][j-weight[i-1]]+cost[i-1]));
+                else
+                    knapsack[i][j]=knapsack[i-1][j];
+//                System.out.println("j:"+j+", knapsack:"+knapsack[i][j]);
+            }
+        }
+        System.out.println("Max Capacity: "+knapsack[6][8]);
+        
+        System.out.println("Trace back the elements of the knapsack bag:");
+    
+        int i=6;
+            for (int j = 8; j >0;) {                
+                if(j-weight[i-1]>0){
+                    if(knapsack[i][j]-knapsack[i-1][j-weight[i-1]]== cost[i-1]){
+                            finalList.add(i);
+                            j-=weight[i-1];
+                     }
+                }
+                i--;                
+                if(i==0 || j<=0)
+                    break;
+            }
+        
+        System.out.println("Final list: "+finalList);
+    
+    }
+}
