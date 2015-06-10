e5956d85ea669be7eec730bc4f6306b1cc9f9ee5
VENKAT RAM REDDY KUNTA <venkat.csulb@gmail.com>
2015-06-09 19:56:00 -0700

    Puzzle Solver: Update: Accomplished counting Tasks: Re-factoring of code

--- Pangrams.java
+++ Pangrams.java
@@ -1,44 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package problemsolving;
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
-
-/**
- *
- * @author venkatramreddykunta
- */
-public class Pangrams {
-    
-    public static void main(String[] args) {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-    
-                Scanner input=new Scanner(System.in);
-                String textInput=Character.parseChar(input.nextLine().split(" "));
-                char alphabet; 
-               int count=0;
-                 HashSet<Character> alphabetSet=new HashSet<>();
-                for(int i=0;i<textInput.length();i++){
-                            alphabet=textInput.charAt(i);
-                      //System.out.println(alphabet);  
-                    if(!alphabetSet.contains(textInput.charAt(i)) && ((alphabet>='a' && alphabet <='z') || (alphabet>='A' && alphabet <='Z') )){
-                        // not in set and an alphabet           
-                          alphabetSet.add(alphabet);
-                           count++;     
-                    }
-                }
-                if(count==26)
-                            System.out.println("pangram");
-                
-                else
-                            System.out.println("not pangram");
-                
-    }
-}
-
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package problemsolving;
+//import java.io.*;
+//import java.util.*;
+//import java.text.*;
+//import java.math.*;
+//import java.util.regex.*;
+//
+///**
+// *
+// * @author venkatramreddykunta
+// */
+//public class Pangrams {
+//    
+//    public static void main(String[] args) {
+//    /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+//    
+//                Scanner input=new Scanner(System.in);
+////                String textInput=Character.parseChar(input.nextLine().split(" "));
+//                char alphabet; 
+//               int count=0;
+//                 HashSet<Character> alphabetSet=new HashSet<>();
+//                for(int i=0;i<textInput.length();i++){
+//                            alphabet=textInput.charAt(i);
+//                      //System.out.println(alphabet);  
+//                    if(!alphabetSet.contains(textInput.charAt(i)) && ((alphabet>='a' && alphabet <='z') || (alphabet>='A' && alphabet <='Z') )){
+//                        // not in set and an alphabet           
+//                          alphabetSet.add(alphabet);
+//                           count++;     
+//                    }
+//                }
+//                if(count==26)
+//                            System.out.println("pangram");
+//                
+//                else
+//                            System.out.println("not pangram");
+//                
+//    }
+//}
+//
--- StringPermutation.java
+++ StringPermutation.java
@@ -3,43 +3,43 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package problemsolving;
-
-import static strings.permutation.Permutation.N;
-
-/**
- *
- * @author venkatramreddykunta
- */
-public class StringPermutation {
-    StringBuffer buffer;
-    int N,i,j,k,value,index;
-    public static void main(String[] args) {
-        StringPermutation stringPermutation=new StringPermutation();
-        stringPermutation.pemute();
-    }
-
-    private void pemute() {
-        buffer=new StringBuffer("koti");
-         N=buffer.length();
-        StringBuffer[] perm=new StringBuffer[(int)Math.pow(N,N)];
-        for(j=0;j<N;j++){                
-                for(i=0;i<(int)Math.pow(N,N-j);i++){                    
-                    for(k=0;k<(int)Math.pow(N,j);k++){                        
-                        value=i/(int)Math.pow(N,N-j-1);
-                        index=k*(int)Math.pow(N,N-j)+i;
-                        perm[index][j]=list[value];                        
-                    }                         
-                }
-        }                            
-        System.out.println("All possible combination of input");       
-        for(i=0;i<(int)Math.pow(N,N);i++){
-            for(j=0;j<N;j++){
-                System.out.print(" "+perm[i][j]);
-            }   
-            System.out.println();
-        }       
-        
-        
-    }
-}
+//package problemsolving;
+//
+//
+//
+///**
+// *
+// * @author venkatramreddykunta
+// */
+//public class StringPermutation {
+//    StringBuffer buffer;
+//    int N,i,j,k,value,index;
+//    public static void main(String[] args) {
+//        StringPermutation stringPermutation=new StringPermutation();
+//        stringPermutation.pemute();
+//    }
+//
+//    private void pemute() {
+//        buffer=new StringBuffer("koti");
+//         N=buffer.length();
+//        StringBuffer[] perm=new StringBuffer[(int)Math.pow(N,N)];
+//        for(j=0;j<N;j++){                
+//                for(i=0;i<(int)Math.pow(N,N-j);i++){                    
+//                    for(k=0;k<(int)Math.pow(N,j);k++){                        
+//                        value=i/(int)Math.pow(N,N-j-1);
+//                        index=k*(int)Math.pow(N,N-j)+i;
+//                        perm[index][j]=list[value];                        
+//                    }                         
+//                }
+//        }                            
+//        System.out.println("All possible combination of input");       
+//        for(i=0;i<(int)Math.pow(N,N);i++){
+//            for(j=0;j<N;j++){
+//                System.out.print(" "+perm[i][j]);
+//            }   
+//            System.out.println();
+//        }       
+//        
+//        
+//    }
+//}
